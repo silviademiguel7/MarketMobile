23d2b5ebb79e7de8cc19f5d001dc5ba834c89e57
silvia <sivlia.demiguel@biko2.com>
2021-02-08 17:11:44 +0100

    añado el listado de moviles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,59 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import "./App.scss";
+import { Filter } from "./components/Filter";
 
-function App() {
+interface Mobiles {
+  brand: string;
+  id: string;
+  imgUrl: string;
+  model: string;
+  price: string;
+}
+
+const App: React.FC = () => {
+  const onLoad = async () => {
+    const { data } = await axios.get(
+      "https://front-test-api.herokuapp.com/api/product "
+    );
+    setMobiles(data);
+    console.log(data);
+  };
+  useEffect(() => {
+    onLoad();
+  }, []);
+
+  const [mobiles, setMobiles] = useState<Mobiles[]>([]);
   return (
     <div className="App-wrapper">
-      <header className="header">
-        <h1>Phone Market</h1>
-      </header>
-      <main></main>
+      <header className="header">Phone Market</header>
+      <main className="main-content">
+        <Filter />
+        <div className="results-container">
+          <ul className="results-list">
+            {mobiles.map((mobile) => {
+              return (
+                <li className="item-list" key={mobile.id}>
+                  <div className="card">
+                    <div className="card__image">
+                      <img src={mobile.imgUrl} />
+                    </div>
+                    <div className="card__data">
+                      <p className="card__data-brand">Marca: {mobile.brand}</p>
+                      <p className="card__data-model">Modelo: {mobile.model}</p>
+                      <p className="card__data-price">
+                        Precio: {mobile.price}€
+                      </p>
+                    </div>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
