d1bc054a68a45792cf324379947cdf89da554177
silvia <sivlia.demiguel@biko2.com>
2021-02-08 15:47:14 +0100

    crea cabecera

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import "./App.scss";
 
 function App() {
-  return <div>"hola"</div>;
+  return (
+    <div className="App-wrapper">
+      <header className="header">
+        <h1>Phone Market</h1>
+      </header>
+      <main></main>
+    </div>
+  );
 }
 
 export default App;
