c8b27eb5a83d93882a89ac8deff37bdcd16e57d7
silvia <sivlia.demiguel@biko2.com>
2021-02-09 17:38:33 +0100

    redirige a landing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import "./App.scss";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import { Landing } from "./Landing";
 
 const App: React.FC = () => {
@@ -11,6 +16,9 @@ const App: React.FC = () => {
       <main className="main-content">
         <Router>
           <Switch>
+            <Route path="/" exact>
+              <Redirect to="/mobiles" />
+            </Route>
             <Route path="/mobiles" exact>
               <Landing />
             </Route>
